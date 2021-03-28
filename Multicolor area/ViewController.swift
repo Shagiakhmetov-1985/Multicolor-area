3baac0b6c051f07f742eea3b49588bed277f6eda
Marat Shagiakhmetov <shagiakhmetov2499@gmail.com>
2021-03-28 18:15:36 +0300

    Completed homework

--- Multicolor area/ViewController.swift
+++ Multicolor area/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var multiViewColor: UIView!
+    
+    @IBOutlet weak var labelRedColor: UILabel!
+    @IBOutlet weak var labelGreenColor: UILabel!
+    @IBOutlet weak var labelBlueColor: UILabel!
+    
+    @IBOutlet weak var sliderRedColor: UISlider!
+    @IBOutlet weak var sliderGreenColor: UISlider!
+    @IBOutlet weak var sliderBlueColor: UISlider!
+    
+    private func setColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        multiViewColor.backgroundColor = .init(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //View
+        multiViewColor.layer.cornerRadius = 15
+        multiViewColor.backgroundColor = .init(red: CGFloat(sliderRedColor.value), green: CGFloat(sliderGreenColor.value), blue: CGFloat(sliderBlueColor.value), alpha: 1)
+        
+        //Sliders
+        //Red
+        sliderRedColor.minimumTrackTintColor = .red
+        sliderRedColor.maximumTrackTintColor = .darkGray
+        //Green
+        sliderGreenColor.minimumTrackTintColor = .green
+        sliderGreenColor.maximumTrackTintColor = .darkGray
+        //Blue
+        sliderBlueColor.minimumTrackTintColor = .blue
+        sliderBlueColor.maximumTrackTintColor = .darkGray
+        
     }
-
+    
+    @IBAction func sliderRedAction() {
+        labelRedColor.text = String(format: "%.2f", sliderRedColor.value)
+        setColor(red: CGFloat(sliderRedColor.value), green: CGFloat(sliderGreenColor.value), blue: CGFloat(sliderBlueColor.value))
+    }
+    
+    @IBAction func sliderGreenAction() {
+        labelGreenColor.text = String(format: "%.2f", sliderGreenColor.value)
+        setColor(red: CGFloat(sliderRedColor.value), green: CGFloat(sliderGreenColor.value), blue: CGFloat(sliderBlueColor.value))
+    }
+    
+    @IBAction func sliderBlueAction() {
+        labelBlueColor.text = String(format: "%.2f", sliderBlueColor.value)
+        setColor(red: CGFloat(sliderRedColor.value), green: CGFloat(sliderGreenColor.value), blue: CGFloat(sliderBlueColor.value))
+    }
 
 }
 
