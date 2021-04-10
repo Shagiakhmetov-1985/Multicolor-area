bd25536131d82fb48e108558c20e9b1c8f26c284
Marat Shagiakhmetov <shagiakhmetov2499@gmail.com>
2021-04-10 17:51:21 +0300

    added textfields

---
+++ Multicolor area/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  Multicolor area
+//
+//  Created by Marat Shagiakhmetov on 09.04.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+    }
+}
--- Multicolor area/MulticolorViewController.swift
+++ Multicolor area/MulticolorViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MulticolorViewController: UIViewController {
     
     @IBOutlet weak var multiViewColor: UIView!
     
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderGreenColor: UISlider!
     @IBOutlet weak var sliderBlueColor: UISlider!
     
+    @IBOutlet weak var redColorTextField: UITextField!
+    @IBOutlet weak var greenColorTextField: UITextField!
+    @IBOutlet weak var blueColorTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +30,7 @@ class ViewController: UIViewController {
         multiViewColor.layer.cornerRadius = 15
         setColor()
         setValue(for: labelRedColor, labelGreenColor, labelBlueColor)
+        setValueTextField(for: redColorTextField, greenColorTextField, blueColorTextField)
         
         //Sliders
         //Red
@@ -40,17 +45,36 @@ class ViewController: UIViewController {
         
     }
     
-    
     @IBAction func rgbSlider(_ sender: UISlider) {
         setColor()
         
         switch sender {
         case sliderRedColor:
             setValue(for: labelRedColor)
+            setValueTextField(for: redColorTextField)
         case sliderGreenColor:
             setValue(for: labelGreenColor)
+            setValueTextField(for: greenColorTextField)
         default:
             setValue(for: labelBlueColor)
+            setValueTextField(for: blueColorTextField)
+        }
+    }
+    
+    @IBAction func rgbTextField(_ sender: UITextField) {
+        switch sender {
+        case redColorTextField:
+            let textField = Float(sender.text!)
+            sliderRedColor.value = textField!
+            rgbSlider(sliderRedColor)
+        case greenColorTextField:
+            let textField = Float(sender.text!)
+            sliderGreenColor.value = textField!
+            rgbSlider(sliderGreenColor)
+        default:
+            let textField = Float(sender.text!)
+            sliderBlueColor.value = textField!
+            rgbSlider(sliderBlueColor)
         }
     }
     
@@ -76,9 +100,29 @@ class ViewController: UIViewController {
         }
     }
     
+    private func setValueTextField(for textField: UITextField...) {
+        textField.forEach { textField in
+            switch textField {
+            case redColorTextField:
+                textField.text = string(from: sliderRedColor)
+            case greenColorTextField:
+                textField.text = string(from: sliderGreenColor)
+            default:
+                textField.text = string(from: sliderBlueColor)
+            }
+        }
+    }
+    
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
     
 }
 
+// MARK: - Work with keyboard
+extension MulticolorViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+}
